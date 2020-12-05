740af8812ac1a56210942bed1fd695bf696223a3
Laika-Beats <laikabeatsdenver@gmail.com>
2020-12-04 19:29:43 -0700

    Navbar + pages setup/basic styling

--- mern-auth-front/src/components/layout/Header.js
+++ mern-auth-front/src/components/layout/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import AuthOptions from "../auth/AuthOptions";
 
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <header id="header">
+            <Link to="/">
+                <h1 className="title">MERN auth todo app</h1>
+            </Link>
+            <AuthOptions />
+        </header>
     )
 }
